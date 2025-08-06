69809c29b390b37e62bebc9213cf21581f396cb7
Daniel Kachisuzi <bit22-dkachisuzi@mubas.ac.mw>
2025-08-06 08:23:32 +0200

    have done Q6

---
+++ Q9.js
@@ -0,0 +1,15 @@
+
+ //continouing of loop
+for(let i = 1;i<10;i++){
+    if(i>0){
+        console.log("num is:",i)
+    }
+}
+
+//breaking of loop
+    for(let i = 2;i<10;i++){
+ if(i>!5){
+        break;
+    }
+   
+}
