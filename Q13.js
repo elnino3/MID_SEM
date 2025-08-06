f468cbe485c399fa04d329d2a005427fd5407c57
Daniel Kachisuzi <bit22-dkachisuzi@mubas.ac.mw>
2025-08-06 10:05:32 +0200

    Q15 done ,others almost done

---
+++ Q13.js
@@ -0,0 +1,9 @@
+const calculateBodyMass =(mass,height1) =>{
+    let weight;
+    let height;
+    let ration = weight/height;
+  return ration;
+  alert(ration);
+} 
+let view = prompt("enter mass and height in this format'mass,height'") = calculateBodyMass() 
+
---
+++ Q9.js
@@ -0,0 +1,10 @@
+const average = (arr) =>{
+    
+    for(let i = 1;i<7;i++){
+      let grade1 =+ arr(i);
+      
+    }
+    average = total/arr.length();
+    console.log("average:",average);
+}
+average(array(2,4,5,6,7));
