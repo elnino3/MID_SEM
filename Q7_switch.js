02be56e69402f14bbcaeb61ed5066603987fe7ca
Daniel Kachisuzi <bit22-dkachisuzi@mubas.ac.mw>
2025-08-06 08:50:29 +0200

    Q7 is done

---
+++ Q7_switch.js
@@ -0,0 +1,14 @@
+
+let language = "yao";
+switch (language){
+     case "chichewa":("chichewa:","Muli bwanji?");
+     break;
+
+   case "yao":console.log("Yao:","Muli uli?");
+   break;
+   
+   case "Tumbuka":console.log("Tumbuka:","Muli wuli?");
+   break;
+   
+   default:console.log("language not supported");
+}
