26826fd9dbfd022451e2fd2c312011c635cd68ff
Daniel Kachisuzi <bit22-dkachisuzi@mubas.ac.mw>
2025-08-06 08:09:34 +0200

    Q3 done

---
+++ Q3.js
@@ -0,0 +1,21 @@
+let num1 = 5;
+let num2 = 10;
+
+console.log("division:",num2/num1); //divisiom
+
+if(num1>!num2){
+console.log("logical NOT:","num1 is less than num2");  //logical NOT
+}
+else console.log("logical NOT :","num2 is less than num1");
+
+ let result = num2<=num1 ? "great":"bad";   //ternary op
+ console.log(result);
+
+ //increment and decrement
+let x = 10;
+ console.log("before increment:", x);
+ x++;
+ console.log("after increment:", x);
+ x--;
+ console.log("after decrement:", x);
+
